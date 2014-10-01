64fb46e15396bf20e7c244905dc7bff58075bb79
Attila Tamasi <attila.tamasi@neofonie.de>
2014-10-01 08:48:42 +0200

    Creating empty project based on Xcode tabbar application template

---
+++ Swift Radio/Swift Radio/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Swift Radio
+//
+//  Created by Attila Tamasi on 01/10/14.
+//  Copyright (c) 2014 csu-csu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
